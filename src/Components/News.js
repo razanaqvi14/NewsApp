f7d97acc331b0ca0867ce18ce990257c9e18fd43
Syed Mohammad Ali Raza Naqvi <razanaqvi2004@gmail.com>
2023-06-11 11:30:27 +0500

    Added infinite scroll

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+import loadingGif from "../assets/loading.gif";
 
 export default class News extends Component {
   static defaultProps = {
@@ -14,169 +15,84 @@ export default class News extends Component {
     category: PropTypes.string,
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
+      totalResults: 0,
       page: 1,
-      loading: false,
-      pageSize: 8,
+      hasMore: true,
     };
+    document.title = `NexusNews | ${this.capitalizeFirstLetter(
+      this.props.category
+    )}`;
   }
 
-  updateNews = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e8603c7a4d9464a817e8d73e79ad3ce&page=${this.state.page}&pageSize=${this.state.pageSize}`;
-    this.setState({
-      loading: true,
-    });
-    let response = await fetch(url);
-    let data = await response.json();
-    this.setState({
-      articles: data.articles,
-      totalArticles: data.totalResults,
-      loading: false,
-    });
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
   async componentDidMount() {
-    this.updateNews();
-  }
-
-  handlePreviousClick = async () => {
-    this.setState({
-      page: this.state.page - 1,
-    });
-    this.updateNews();
-  };
-
-  handleNextClick = async () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-    this.updateNews();
-  };
-
-  selectedPageSize = async (numberOfArticles) => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=5e8603c7a4d9464a817e8d73e79ad3ce&page=1&pageSize=${
-      this.state.pageSize - this.state.pageSize + numberOfArticles
-    }`;
-    this.setState({
-      loading: true,
-    });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e8603c7a4d9464a817e8d73e79ad3ce&page=${this.state.page}&pageSize=${this.state.pageSize}`;
     let response = await fetch(url);
     let data = await response.json();
     this.setState({
       articles: data.articles,
-      totalArticles: data.totalResults,
-      loading: false,
+      totalResults: data.totalResults,
     });
+  }
+
+  fetchMore = async () => {
+    if (this.state.articles.length <= this.state.totalResults) {
+      this.setState({
+        page: this.state.page + 1,
+      });
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e8603c7a4d9464a817e8d73e79ad3ce&page=${this.state.page}&pageSize=${this.state.pageSize}`;
+      let response = await fetch(url);
+      let data = await response.json();
+      this.setState({
+        articles: this.state.articles.concat(data.articles),
+        totalResults: data.totalResults,
+      });
+    } else {
+      this.setState({
+        hasMore: false,
+      });
+    }
   };
 
   render() {
     return (
-      <div className="container my-4 flex flex-col justify-center align-middle">
-        <div
-          className="flex justify-between mb-4"
-          style={{ alignItems: "center" }}
+      <div className="container my-4">
+        <h1 className="text-3xl font-semibold bg-slate-900 text-white w-fit p-2.5 rounded-xl mb-4">
+          NexusNews - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+          Headlines
+        </h1>
+        <InfiniteScroll
+          style={{ padding: "1px 17px" }}
+          dataLength={this.state.articles.length}
+          next={this.fetchMore}
+          hasMore={this.state.hasMore}
+          loader={
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img style={{ width: "40px" }} src={loadingGif} alt="" />
+            </div>
+          }
+          endMessage={
+            <h5 className="font-semibold text-center">
+              You have reached the end of this page
+            </h5>
+          }
         >
-          <h1 className="text-3xl font-semibold bg-slate-900 text-white w-fit p-2.5 rounded-xl">
-            NexusNews - Top headlines of category "{this.props.category}"
-          </h1>
-          <div
-            className="dropdown flex flex-col justify-center"
-            style={{ alignItems: "center" }}
-          >
-            <p className="mb-2 font-semibold">
-              Default number of articles per page is 8
-            </p>
-            <button
-              style={{ width: "54%" }}
-              className="btn btn-dark bg-slate-900 hover:bg-slate-700 text-white dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Articles Per Page
-            </button>
-            <ul className="dropdown-menu">
-              <li
-                onClick={() => {
-                  this.selectedPageSize(4);
-                }}
-              >
-                <a
-                  className="dropdown-item hover:bg-slate-900 hover:text-white"
-                  href="#"
+          <div className="row mt-4">
+            {this.state.articles.map((article, i) => {
+              return (
+                <div
+                  className="col mb-4 flex justify-center align-middle"
+                  key={i}
                 >
-                  4
-                </a>
-              </li>
-              <li
-                onClick={() => {
-                  this.selectedPageSize(8);
-                }}
-              >
-                <a
-                  className="dropdown-item hover:bg-slate-900 hover:text-white"
-                  href="#"
-                >
-                  8
-                </a>
-              </li>
-              <li
-                onClick={() => {
-                  this.selectedPageSize(12);
-                }}
-              >
-                <a
-                  className="dropdown-item hover:bg-slate-900 hover:text-white"
-                  href="#"
-                >
-                  12
-                </a>
-              </li>
-              <li
-                onClick={() => {
-                  this.selectedPageSize(16);
-                }}
-              >
-                <a
-                  className="dropdown-item hover:bg-slate-900 hover:text-white"
-                  href="#"
-                >
-                  16
-                </a>
-              </li>
-              <li
-                onClick={() => {
-                  this.selectedPageSize(20);
-                }}
-              >
-                <a
-                  className="dropdown-item hover:bg-slate-900 hover:text-white"
-                  href="#"
-                >
-                  20
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="row">
-          {this.state.articles.map((article) => {
-            return (
-              <div
-                className="col mb-4 flex justify-center align-middle"
-                key={article.url}
-              >
-                {this.state.loading ? (
-                  <Spinner />
-                ) : (
                   <NewsItem
+                    key={i}
                     title={article.title !== null ? article.title : ""}
                     description={
                       article.description !== null ? article.description : ""
@@ -187,34 +103,11 @@ export default class News extends Component {
                     author={article.author}
                     source={article.source.name}
                   />
-                )}
-              </div>
-            );
-          })}
-        </div>
-
-        <div className="flex justify-around" style={{ alignItems: "center" }}>
-          <button
-            className="btn btn-dark bg-slate-900 hover:bg-slate-700"
-            onClick={this.handlePreviousClick}
-            disabled={this.state.page === 1}
-          >
-            &larr; Previous
-          </button>
-          <div className="text-base font-semibold">
-            Page No. {this.state.page}
+                </div>
+              );
+            })}
           </div>
-          <button
-            className="btn btn-dark bg-slate-900 hover:bg-slate-700"
-            onClick={this.handleNextClick}
-            disabled={
-              this.state.page ===
-              Math.ceil(this.state.totalArticles / this.state.pageSize)
-            }
-          >
-            Next &rarr;
-          </button>
-        </div>
+        </InfiniteScroll>
       </div>
     );
   }
